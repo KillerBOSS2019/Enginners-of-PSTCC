58252e2120ca28e6f23e0c9faa00fa80eb818f99
Damien Staebler <damienweifen@gmail.com>
2024-11-02 22:41:22 -0400

    basically done

---
+++ src/app/fonts.ts
@@ -0,0 +1,18 @@
+import localFont from "next/font/local";
+
+export const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+export const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const squadaOne = localFont({
+  src: "./fonts/SquadaOne-Regular.ttf",
+  variable: "--font-squada-one",
+  weight: "64",
+});
