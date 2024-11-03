c732a8ff96bf783311e7d98488979cd59d4b19be
Damien Staebler <damienweifen@gmail.com>
2024-11-03 16:37:07 -0500

    Update sponsors.tsx

--- src/app/sections/sponsors.tsx
+++ src/app/sections/sponsors.tsx
@@ -6,7 +6,7 @@ import { sponsors } from '../data/sponsors.json';
 export function Sponsors() {
     return (
         <>
-            <SectionBanner id="sponsors" title="Our Sponsors" description='Thank You Wonderful Sponsors!' />
+            <SectionBanner id="sponsors" title="Our Sponsors" description='Thank You Wonderful Sponsors!' children={undefined} />
             <Section bgImage={undefined} bgColor={"#003882"}>
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 px-4">
                     {sponsors.map((sponsor, index) => (
